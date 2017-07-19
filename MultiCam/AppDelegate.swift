a8e3c821bf13be053d2cbfe4fdba4b0eb4da7dc1
Andreas Pohl <andreas.g.pohl@gmail.com>
2017-07-19 12:17:20 +0200

    Initial Commit

---
+++ MultiCam/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MultiCam
+//
+//  Created by Andreas on 19.07.17.
+//  Copyright © 2017 Andreas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MultiCam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MultiCam
+//
+//  Created by Andreas on 19.07.17.
+//  Copyright © 2017 Andreas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
