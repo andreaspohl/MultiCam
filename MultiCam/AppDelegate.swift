747b1392a1c54125228d9a8bd778999eb76b3182
Andreas Pohl <andreas.g.pohl@gmail.com>
2017-07-19 12:18:38 +0200

    Initial commit

--- MultiCam/AppDelegate.swift
+++ MultiCam/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        // Insert code here to tear down your application 
     }
 
 
